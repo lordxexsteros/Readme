4f20da9553cc260f7a275bc0e1af8336c0bd3a6a
lordxexsteros <code.xexsteros@gmail.com>
2024-04-27 08:04:19 +0530

    New pc breaker

---
+++ PC breaker.js
@@ -0,0 +1,13 @@
+
+//BE CareFull!
+       // THis is a pc breaker
+       
+       let breaker = () =>{for(;;) {
+           
+           console.log("oh nooo")
+           
+        }}
+        // What is a breaker?
+        // It is a function that will run forever until the pc crashes.
+      
+        console.log(breaker)     
